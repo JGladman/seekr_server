3458fedfdd2dc842dc0241d3e13d7867cb4cf3fc
Jacob <jacobgladman32@gmail.com>
2021-01-23 02:09:41 -0500

    Added mongoDB connection

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const cors = require('cors');
@@ -12,10 +13,22 @@ app.use(
     extended: true,
   }),
 );
+
 const port = 3001;
 
+mongoose.connect(process.env.CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('connected to mongoDB');
+});
+
 app.get('/', (req, res) => {
-  res.json({ info: 'Node.js, Express, and MongoDB API' });
+  res.json({ info: 'Hello world' });
 });
 
 app.listen(port, () => {
